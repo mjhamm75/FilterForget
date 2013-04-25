c26396f9237d38d4077d97e881096601efae9c00
Jason Hamm <mhamm@availity.com>
2013-04-25 06:11:45 -0400

    Initial Commit

---
+++ src/com/app/filterforget/FilterActivity.java
@@ -0,0 +1,15 @@
+package com.app.filterforget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FilterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_filter);
+	}
+	
+}
---
+++ src/com/app/filterforget/MainActivity.java
@@ -0,0 +1,32 @@
+package com.app.filterforget;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Resources res = getResources(); // Resource object to get Drawables
+		TabHost tabHost = getTabHost(); // The activity TabHost
+		TabHost.TabSpec spec; // Reusable TabSpec for each tab
+		Intent intent; // Reusable Intent for each tab
+
+		// Create an Intent to launch an Activity for the tab (to be reused)
+		intent = new Intent().setClass(this, FilterActivity.class);
+		spec = tabHost.newTabSpec("activity_filter").setIndicator("Filter").setContent(intent);
+		tabHost.addTab(spec);
+
+		intent = new Intent().setClass(this, InfoActivity.class);
+		spec = tabHost.newTabSpec("activity_info").setIndicator("Info").setContent(intent);
+		tabHost.addTab(spec);
+
+		tabHost.setCurrentTab(0);
+	}
+
+}
