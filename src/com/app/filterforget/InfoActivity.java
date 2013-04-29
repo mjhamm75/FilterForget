8d274877dce3e8a72ffd6429158e9eada44307fd
Jason Hamm <mhamm@availity.com>
2013-04-29 07:13:16 -0400

    Broadcast Receiver now working

--- src/com/app/filterforget/InfoActivity.java
+++ src/com/app/filterforget/InfoActivity.java
@@ -1,22 +1,54 @@
 package com.app.filterforget;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TimePicker;
+import android.view.View;
+import android.widget.Toast;
 
 public class InfoActivity extends Activity {
+	// private OrderAlarmBroadcastReceiver alarm;
+	// private Context context = this;
+	//
+	// @Override
+	// protected void onCreate(Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.activity_info);
+	//
+	// alarm = new OrderAlarmBroadcastReceiver();
+	//
+	// TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+	// timePicker.setOnTimeChangedListener(new
+	// TimePicker.OnTimeChangedListener() {
+	// @Override
+	// public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+	// System.out.println(hourOfDay);
+	// System.out.println(minute);
+	// }
+	// });
+	//
+	// Button alarmButton = (Button) findViewById(R.id.alarm);
+	// alarmButton.setOnClickListener(new View.OnClickListener() {
+	// @Override
+	// public void onClick(View v) {
+	// alarm.setAlarm(context);
+	// }
+	// });
+	// }
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
-		
-		TimePicker timePicker = (TimePicker)findViewById(R.id.timePicker);
-		timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {			
-			@Override
-			public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-				System.out.println(hourOfDay);
-				System.out.println(minute);				
-			}
-		});
+	}
+
+	public void startAlert(View view) {
+		Intent intent = new Intent(this, OrderAlarmBroadcastReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (5 * 1000), pendingIntent);
+		Toast.makeText(this, "Alarm set in " + 5 + " seconds", Toast.LENGTH_LONG).show();
 	}
 }
