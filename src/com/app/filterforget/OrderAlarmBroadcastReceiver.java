a059b40b12640172cecb736d952ef45bfa644b79
Jason Hamm <mhamm@availity.com>
2013-05-01 06:33:12 -0400

    Add ability for BroadcastReceiver to open new activity

--- src/com/app/filterforget/OrderAlarmBroadcastReceiver.java
+++ src/com/app/filterforget/OrderAlarmBroadcastReceiver.java
@@ -10,5 +10,9 @@ public class OrderAlarmBroadcastReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Toast.makeText(context, "Time UP", Toast.LENGTH_LONG).show();
+		Intent i = new Intent();
+		i.setClassName("com.app.filterforget", "com.app.filterforget.FilterOrderPopup");
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(i);
 	}
 }
