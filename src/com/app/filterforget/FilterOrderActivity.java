341584a4303e49858f207244654d40ce85476038
Jason Hamm <mhamm@availity.com>
2013-05-07 06:52:31 -0400

    List to show select filters to be ordered

---
+++ src/com/app/filterforget/FilterOrderActivity.java
@@ -0,0 +1,13 @@
+package com.app.filterforget;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class FilterOrderActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
