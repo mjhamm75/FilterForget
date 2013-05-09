638bdbcddc5212c747719a27f8f133da645a33fb
Jason Hamm <mhamm@availity.com>
2013-05-09 06:43:04 -0400

    Add button handlers for order activity

--- src/com/app/filterforget/FilterOrderActivity.java
+++ src/com/app/filterforget/FilterOrderActivity.java
@@ -1,12 +1,33 @@
 package com.app.filterforget;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class FilterOrderActivity extends ContainerActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	}
+		setContentView(R.layout.activity_order);
 
+		ListView filterList = (ListView) findViewById(R.id.list_view_order_filter);
+
+		Button order = (Button) findViewById(R.id.orderFilters);
+		order.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.println("ORDER");
+			}
+		});
+
+		Button cancel = (Button) findViewById(R.id.orderFiltersCancel);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.println("CANCEL");
+			}
+		});
+	}
 }
