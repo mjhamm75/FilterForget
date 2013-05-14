c7235a43fc122bafa4ee42686d57458abc43a93d
Jason Hamm <mhamm@availity.com>
2013-05-14 06:01:50 -0400

    Add cancel action to add credit card dialog

--- src/com/app/filterforget/InfoActivity.java
+++ src/com/app/filterforget/InfoActivity.java
@@ -3,6 +3,7 @@ package com.app.filterforget;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 
 public class InfoActivity extends ContainerActivity {
@@ -16,9 +17,19 @@ public class InfoActivity extends ContainerActivity {
 		addCreditCard.setOnClickListener(new View.OnClickListener() {		
 			@Override
 			public void onClick(View v) {			
-				Dialog dialog = new Dialog(InfoActivity.this);
+				final Dialog dialog = new Dialog(InfoActivity.this);
+				dialog.getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 				dialog.setContentView(R.layout.dialog_add_credit_card);
 				dialog.setTitle(R.string.card_info);
+				
+				Button addCard = (Button)dialog.findViewById(R.id.buttonAddCard);
+				Button addCardCancel = (Button)dialog.findViewById(R.id.buttonAddCardCancel);
+				addCardCancel.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				});
 				dialog.show();
 			}
 		});
