3415520cd40dc84280d5e71afb75e6048c96c752
Jason Hamm <mhamm@availity.com>
2013-05-14 05:44:16 -0400

    Get ready to add new credit card dialog

--- src/com/app/filterforget/InfoActivity.java
+++ src/com/app/filterforget/InfoActivity.java
@@ -1,10 +1,9 @@
 package com.app.filterforget;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class InfoActivity extends ContainerActivity {
 	
@@ -12,5 +11,13 @@ public class InfoActivity extends ContainerActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
+		
+		Button addCreditCard = (Button)findViewById(R.id.button_add_card);
+		addCreditCard.setOnClickListener(new View.OnClickListener() {		
+			@Override
+			public void onClick(View v) {			
+				Dialog dialog = new Dialog(InfoActivity.this);
+			}
+		});
 	}
 }
