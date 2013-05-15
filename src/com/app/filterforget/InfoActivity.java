43373baa63f38e4e34d06d4ee51d91b09978872e
Jason Hamm <mhamm@availity.com>
2013-05-15 06:47:02 -0400

    Add datepicker for exp date

--- src/com/app/filterforget/InfoActivity.java
+++ src/com/app/filterforget/InfoActivity.java
@@ -1,11 +1,16 @@
 package com.app.filterforget;
 
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class InfoActivity extends ContainerActivity {
 
@@ -19,15 +24,35 @@ public class InfoActivity extends ContainerActivity {
 			@Override
 			public void onClick(View v) {
 				final Dialog dialog = new Dialog(InfoActivity.this);
+				final Calendar dateTime = Calendar.getInstance();
 				dialog.setContentView(R.layout.dialog_add_credit_card);
 				dialog.getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
 				dialog.setTitle(R.string.card_info);
+
+				ImageButton expDate = (ImageButton) dialog.findViewById(R.id.buttonExpDate);
+				expDate.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+
+						new DatePickerDialog(v.getContext(), d, dateTime.get(Calendar.YEAR), dateTime
+								.get(Calendar.MONTH), dateTime.get(Calendar.DAY_OF_MONTH)).show();
+					}
+
+					DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+						@Override
+						public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+							dateTime.set(Calendar.YEAR, year);
+							dateTime.set(Calendar.MONTH, monthOfYear);
+							dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+						}
+					};
+				});
 
 				Button addCard = (Button) dialog.findViewById(R.id.buttonAddCard);
 				addCard.setOnClickListener(new View.OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						EditText cardNumber = (EditText)dialog.findViewById(R.id.creditCardNumber);
+						EditText cardNumber = (EditText) dialog.findViewById(R.id.creditCardNumber);
 						String number = cardNumber.getText().toString();
 
 					}
